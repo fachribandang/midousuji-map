4b8c719c70c05de29f7aae53d8b417b37823f2a4
fachribandang <fachri.muhammad.tif419@polban.ac.id>
2023-01-22 17:59:20 +0700

    編集ページ追加

--- index.php
+++ index.php
@@ -14,12 +14,14 @@ include "server.php"; ?>
     <header>
       <nav class="navbar navbar-dark bg-dark navbar-expand-lg fixed-top">
         <div class="container-fluid">
-          <h1 class="navbar-brand py-0 my-0">避難場所の受入状況</h1>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active border-start" aria-current="page" href="?page=edit">受入状況編集</a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link active border-start" aria-current="page" href="?page=zone-1">ゾーン⓵</a>
               </li>
@@ -51,6 +53,9 @@ include "server.php"; ?>
         break;
       case "zone-4":
         include 'page/zone-4.php';
+        break;
+      case "edit":
+        include 'page/edit.php';
         break;
       default:
         include 'page/error.php';
---
+++ page/edit.php
@@ -0,0 +1,5 @@
+<div class="container mt-5 py-4 row mx-auto">
+  <div class="col-12 mb-3">
+    <span class="text-success border-bottom border-3 border-success fs-1 pb-2">受入状況編集</span>
+  </div>
+</div>
